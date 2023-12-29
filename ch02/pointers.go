c2b5ba0335f38bcef47fb2de2180278f53da8de6
noctua99 <jonginchoi99@dcn.ssu.ac.kr>
2023-12-29 16:49:35 +0900

    12/29/2023

--- ch02/pointers.go
+++ ch02/pointers.go
@@ -24,4 +24,35 @@ func bothPointers(x *float64) *float64 {
 func main() {
 	var f float64 = 12.123
 	fmt.Println("Memory address of f:", &f)
+
+	// Pointer of f
+	fP := &f
+	fmt.Println("Memory address of f:", fP)
+	fmt.Println("Value of f:", *fP)
+	// The value of f changes
+	processPointer(fP)
+	fmt.Printf("Value of f: %.2f\n", f)
+
+	// The value of f does not change
+	x := returnPointer(f)
+	fmt.Printf("Value of x: %.2f\n", *x)
+
+	// The value of f does not change
+	xx := bothPointers(fP)
+	fmt.Printf("Value of xx: %.2f\n", *xx)
+
+	// Check for empty structures
+	var k *aStructure
+
+	// This is nil because currently k points to nowhere
+	fmt.Println(k)
+	// Therefore you are allowed to do this:
+	if k == nil {
+		k = new(aStructure)
+	}
+
+	fmt.Printf("%+v\n", k)
+	if k != nil {
+		fmt.Println("k is not nil")
+	}
 }
